d42a1d76506ad8049a3e30bf1f7344a3225fd172
boxmein <wsdf294@gmail.com>
2015-05-11 00:46:07 +0300

    remove express boilerplate, change view dir

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(favicon());
 app.use(logger('dev'));
 
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
